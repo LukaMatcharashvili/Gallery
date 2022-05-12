957f1f729faef150ea63f40f511d3026aa9775b0
Luka Macharashvili <98055933+LukaMacharashvili@users.noreply.github.com>
2022-05-12 06:51:13 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,16 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCwP4PxS4jHMfi9h9uIUFpzw6zpI5T3Dy4",
-    authDomain: "gallery-c305a.firebaseapp.com",
-    databaseURL: "https://gallery-c305a-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "gallery-c305a",
-    storageBucket: "gallery-c305a.appspot.com",
-    messagingSenderId: "1009876472615",
-    appId: "1:1009876472615:web:84f449b48eb10c6b03cf02",
-    measurementId: "G-B6N7SLF8PP"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
   },
-  cloudinaryBaseUrl: 'https://api.cloudinary.com/v1_1/dr9pyz8sz/upload'
+  cloudinaryBaseUrl: 'cloudinaryBaseUrl'
 };
 
 /*
